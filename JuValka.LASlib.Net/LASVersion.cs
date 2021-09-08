59305ed626dace103e1a36e6a28a72ac42529c08
JuValka <always_zh@qq.com>
2021-09-08 11:13:50 +0800

    ...
    仅读取格式 LAS1.2 FormatID 3  的数据

---
+++ JuValka.LASlib.Net/LASVersion.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace JuValka.LASlib.Net
+{
+    public class LASReadVersion
+    {
+        public string GetVersion(string Las)
+        {
+            using (Stream s = new FileStream(Las, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader br = new BinaryReader(s);
+                br.BaseStream.Seek(0, 0);
+                br.ReadBytes(24);
+                var VersionMajor = br.ReadByte();
+                var VersionMinor = br.ReadByte();
+                var version = VersionMajor.ToString() + "." + VersionMinor.ToString();
+                return version;
+            }
+        }
+        public string VarHeader(string las)
+        {
+            LASReadVersion lasVersion = new LASReadVersion();
+            var strVer = lasVersion.GetVersion(las);
+            return strVer;
+        }
+    }
+}
